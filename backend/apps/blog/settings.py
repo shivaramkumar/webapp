f630bd4178c1a98c9bb40963330378297940cab5
shivaramkumar <connect.shivaram@gmail.com>
2023-10-07 21:43:05 +0000

    working copy

--- backend/apps/blog/settings.py
+++ backend/apps/blog/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "blog.urls"
+ROOT_URLCONF = "apps.blog.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "blog.wsgi.application"
+WSGI_APPLICATION = "apps.blog.wsgi.application"
 
 
 # Database
--- backend/apps/content_management/apps.py
+++ backend/apps/content_management/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ContentManagementConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "content_management"
+    name = "apps.content_management"
